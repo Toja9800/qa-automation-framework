1b512084de0ed6e3da1096aba6618956165344f5
Krystian <bulat9800@gmail.com>
2026-07-12 00:35:18 +0200

    Add shouldTypeIntoUsernameField & shouldFailLoginWithFakeCredentials tests

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.setAllowExtractingPrivateFields;
 
 class FirstSeleniumTest {
     WebDriver driver;
@@ -16,18 +18,44 @@ class FirstSeleniumTest {
         driver = new ChromeDriver();
     }
 
-    @Test
-    void shouldOpenBrowser() {
-        driver.get("https://the-internet.herokuapp.com");
-        System.out.println(driver.getTitle());
-    }
+//    @Test
+//    void shouldOpenBrowser() {
+//        driver.get("https://the-internet.herokuapp.com");
+//        System.out.println(driver.getTitle());
+//    }
+
+//    @Test
+//    void shouldReadPageHeading() {
+//        driver.get("https://the-internet.herokuapp.com");
+//        WebElement heading = driver.findElement(By.tagName("h1"));
+//        System.out.println(heading.getText());
+//    }
+
+//    @Test
+//    void shouldTypeIntoUsernameField() {
+//        driver.get("https://the-internet.herokuapp.com/login");
+//        WebElement usernameField = driver.findElement(By.id("username"));
+//        usernameField.clear();
+//        usernameField.sendKeys("tomek");
+//        System.out.println(usernameField.getAttribute("value"));
+//        assertThat(usernameField.getAttribute("value")).isEqualTo("tomek");
+//    }
 
     @Test
-    void shouldReadPageHeading() {
-        driver.get("https://the-internet.herokuapp.com");
-        WebElement heading = driver.findElement(By.tagName("h1"));
-        System.out.println(heading.getText());
+    void shouldFailLoginWithFakeCredentials() {
+        driver.get("https://the-internet.herokuapp.com/login");
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        usernameField.clear();
+        passwordField.clear();
+        usernameField.sendKeys("tomek");
+        passwordField.sendKeys("hasloTomka123");
+        WebElement loginButton = driver.findElement(By.cssSelector("button.radius"));
+        loginButton.click();
+        WebElement flashMessage = driver.findElement(By.id("flash"));
+        assertThat(flashMessage.getText()).contains("Your username is invalid!");
     }
+
 
     @AfterEach
     void tearDown() {
